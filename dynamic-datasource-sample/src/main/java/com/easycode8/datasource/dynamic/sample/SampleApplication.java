c629092b4dbf953a52dd47bcb8785df9619f6b19
easycode8 <you@example.com>
2023-04-04 11:17:27 +0800

    【优化】使用springfox-swagger2升级2.7.0->2.10.5

--- dynamic-datasource-sample/src/main/java/com/easycode8/datasource/dynamic/sample/SampleApplication.java
+++ dynamic-datasource-sample/src/main/java/com/easycode8/datasource/dynamic/sample/SampleApplication.java
@@ -3,10 +3,10 @@ package com.easycode8.datasource.dynamic.sample;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 
-@EnableSwagger2
+@EnableSwagger2WebMvc //2.10.5版本用于替换@EnableSwagger2
 @SpringBootApplication
 @MapperScan("com.easycode8.datasource.dynamic.sample.mapper")
 public class SampleApplication {
