9468c58435812387a334ab1b00f28d0b939117d8
easycode8 <you@example.com>
2023-08-23 16:55:37 +0800

    【优化】dynamic-datasource-core数据源个性化druid配置合并优化:BeanUtils.copyProperties所有属性暂时修改为只复制connect-properties配置

--- dynamic-datasource-core/src/main/java/com/easycode8/datasource/dynamic/core/creator/DruidDataSourceCreator.java
+++ dynamic-datasource-core/src/main/java/com/easycode8/datasource/dynamic/core/creator/DruidDataSourceCreator.java
@@ -2,12 +2,10 @@ package com.easycode8.datasource.dynamic.core.creator;
 
 import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.pool.DruidDataSource;
-
 import com.easycode8.datasource.dynamic.core.DataSourceInfo;
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.core.env.Environment;
 
@@ -15,7 +13,7 @@ import javax.sql.DataSource;
 import java.util.List;
 
 
-public class DruidDataSourceCreator extends AbstractDataSourceCreator{
+public class DruidDataSourceCreator extends AbstractDataSourceCreator {
     private static final Logger LOGGER = LoggerFactory.getLogger(DruidDataSourceCreator.class);
     public static final String TYPE_CLASS = "com.alibaba.druid.pool.DruidDataSource";
     private final Environment environment;
@@ -30,15 +28,16 @@ public class DruidDataSourceCreator extends AbstractDataSourceCreator{
 
     @Override
     public DataSource doCreateDataSource(DataSourceInfo dataSourceInfo) throws Exception {
-        String prefix = "spring.datasource.dynamic.datasource." + dataSourceInfo.getKey()+ ".druid";
-        DruidDataSource dataSource = Binder.get(environment).bind(prefix, DruidDataSource.class).orElse(null);
-        DruidDataSource defaultDruid = Binder.get(environment).bindOrCreate("spring.datasource.druid", DruidDataSource.class);
-        if (dataSource == null) {
-            LOGGER.info("【easy-model】动态数据源--{}未指定druid配置:{},使用默认druid starter配置", dataSourceInfo.getKey(), prefix);
-            dataSource = defaultDruid;
+        String prefix = "spring.datasource.dynamic.datasource." + dataSourceInfo.getKey() + ".druid";
+        DruidDataSource owner = Binder.get(environment).bind(prefix, DruidDataSource.class).orElse(null);
+        DruidDataSource dataSource = Binder.get(environment).bindOrCreate("spring.datasource.druid", DruidDataSource.class);
+        if (owner == null) {
+            LOGGER.info("[dynamic-datasource] 数据源{}未指定druid配置:{},使用默认druid starter配置", dataSourceInfo.getKey(), prefix);
         } else {
             // 使用单个数据源下个性化配置覆盖默认的
-            BeanUtils.copyProperties(dataSource, Binder.get(environment).bindOrCreate("spring.datasource.druid", DruidDataSource.class));
+            LOGGER.info("[dynamic-datasource] 数据源{}指定druid配置:{},使用合并配置", dataSourceInfo.getKey(), prefix);
+//            BeanUtils.copyProperties(owner, dataSource,"maxIdle", "validConnectionCheckerClass");
+            dataSource.setConnectProperties(owner.getConnectProperties());
         }
         dataSource.setUrl(dataSourceInfo.getUrl());
         dataSource.setUsername(dataSourceInfo.getUsername());
